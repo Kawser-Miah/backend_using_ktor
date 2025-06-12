f0ce975c49b341ac8aa29776db29f0eb4c5b6bc1
kawser-miah <kawsermiah658@gmail.com>
2025-06-12 13:47:14 +0600

    feat: Add request validation plugin
    This commit introduces the `configureRequestValidation` plugin to the application.
    The plugin is now called during the application's module configuration to enable request validation functionality.

---
+++ src/main/kotlin/plugins/RequestValidation.kt
@@ -0,0 +1,11 @@
+package com.example.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.requestvalidation.RequestValidation
+
+fun Application.configureRequestValidation(){
+    install(RequestValidation){
+
+    }
+}
