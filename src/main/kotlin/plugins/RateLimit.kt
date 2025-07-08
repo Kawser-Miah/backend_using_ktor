f71a37eb7699b9a1a1760d2c20005f5dacba4657
Kawser Miah <kawsermiah658@gmail.com>
2025-07-08 23:16:04 +0600

    feat: Handle 429 Too Many Requests in StatusPages
    This commit enhances the `configureStatusPage` plugin to specifically handle `HttpStatusCode.TooManyRequests`.
    
    When a 429 status is encountered, the server will now respond with a custom message indicating the client has sent too many requests and should wait for the duration specified in the "Retry-After" header.

---
+++ src/main/kotlin/plugins/RateLimit.kt
@@ -0,0 +1,25 @@
+package com.example.plugins
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.ratelimit.RateLimit
+import io.ktor.server.plugins.ratelimit.RateLimitName
+import kotlin.time.Duration.Companion.seconds
+
+fun Application.configureRateLimit(){
+    install(RateLimit){
+//        global { rateLimiter(limit = 5, refillPeriod = 60.seconds) } }
+        register(RateLimitName("public")) { rateLimiter(limit = 5, refillPeriod = 60.seconds) }
+        register(RateLimitName("private")) { rateLimiter(limit = 10, refillPeriod = 60.seconds)
+            requestKey { call ->
+                call.request.queryParameters["type"] ?: ""
+            }
+            requestWeight { call, key ->
+                when (key) {
+                    "admin" -> 2
+                    else -> 1
+                }
+            }
+
+        } }
+    }
