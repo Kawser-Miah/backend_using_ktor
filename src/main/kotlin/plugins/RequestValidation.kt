b8bead5b324be8c11d37b9cc6379d3e157f49858
Kawser Miah <kawsermiah658@gmail.com>
2025-07-08 21:57:01 +0600

    feat: Add basic string validation
    This commit introduces a basic string validation rule within the `configureRequestValidation` plugin.
    
    The validation checks if a string body:
    - Is not blank.
    - Starts with "Hello".
    
    If either condition is not met, a `ValidationResult.Invalid` is returned with an appropriate error message. Otherwise, `ValidationResult.Valid` is returned.

--- src/main/kotlin/plugins/RequestValidation.kt
+++ src/main/kotlin/plugins/RequestValidation.kt
@@ -3,9 +3,15 @@ package com.example.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.requestvalidation.RequestValidation
+import io.ktor.server.plugins.requestvalidation.ValidationResult
 
 fun Application.configureRequestValidation(){
     install(RequestValidation){
+        validate<String> {body->
+            if (body.isBlank()) ValidationResult.Invalid("Body should not be empty")
+            else if(!body.startsWith("Hello")) ValidationResult.Invalid("Body should start with Hello")
+            else ValidationResult.Valid
+        }
 
     }
 }
