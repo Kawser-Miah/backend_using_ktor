dd16152bf6669193857496ad909ee0d1fd7b8a7b
Kawser Miah <kawsermiah658@gmail.com>
2025-07-09 19:35:35 +0600

    feat: Implement "unlimited" types for rate limiting
    This commit introduces a set of "unlimited" types (`internal`, `trusted`) to the rate limiting configuration.
    
    Requests with a `type` query parameter matching one of these unlimited types will now have a request weight of 0, effectively bypassing rate limits.

--- src/main/kotlin/plugins/RateLimit.kt
+++ src/main/kotlin/plugins/RateLimit.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.install
 import io.ktor.server.plugins.ratelimit.RateLimit
 import io.ktor.server.plugins.ratelimit.RateLimitName
 import kotlin.time.Duration.Companion.seconds
+val unlimitedTypes = setOf("internal", "trusted")
 
 fun Application.configureRateLimit(){
     install(RateLimit){
@@ -12,10 +13,12 @@ fun Application.configureRateLimit(){
         register(RateLimitName("public")) { rateLimiter(limit = 5, refillPeriod = 60.seconds) }
         register(RateLimitName("private")) { rateLimiter(limit = 10, refillPeriod = 60.seconds)
             requestKey { call ->
-                call.request.queryParameters["type"] ?: ""
+               call.request.queryParameters["type"] ?: ""
+
             }
             requestWeight { call, key ->
                 when (key) {
+                    in unlimitedTypes -> 0
                     "admin" -> 2
                     else -> 1
                 }
